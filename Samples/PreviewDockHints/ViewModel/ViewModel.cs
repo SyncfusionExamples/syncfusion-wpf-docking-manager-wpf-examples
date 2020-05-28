76f091a205828a53f3e0b74bceb56d863a6c19cf
prabakaran.sangameswaran <prabakaran.sangameswaran@syncfusion.com>
2020-05-28 12:09:28 +0530

    Docking manager sample committed

---
+++ Samples/PreviewDockHints/ViewModel/ViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Syncfusion.Windows.Shared;
+using System;
+using System.Windows.Input;
+
+namespace DockingManager_PreviewDockHints_Event
+{
+    class ViewModel
+    {
+        private ICommand previewDockHintsCommand;
+
+        public ICommand PreviewDockHintsCommand
+        {
+            get
+            {
+                return previewDockHintsCommand;
+            }
+        }
+        public ViewModel()
+        {
+            previewDockHintsCommand = new DelegateCommand<object>(previewDockHintsChanged);
+        }
+
+        private void previewDockHintsChanged(object obj)
+        {
+            //if (e.DraggingTarget == Output)
+            //{
+            //    e.DockAbility = DockAbility.Top;
+            //    e.OuterDockAbility = OuterDockAbility.Top;
+            //}
+        }
+    }
+}

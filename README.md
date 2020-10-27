d839d3870119dde4da943467bc0a25e8f3e82480
prabakaran.sangameswaran <prabakaran.sangameswaran@syncfusion.com>
2020-10-27 11:43:06 +0530

    DocumentTabOrdering sample committed.

--- README.md
+++ README.md
@@ -7,6 +7,10 @@ This repository contains the samples that demonstrate the functionalities of Doc
   <td><a href="Samples/PreviewDockHints">Restrict docking hints</a></td>
   <td><a href="Samples/Build-Run">Build and Run mode like VisualStudio</a></td>
   <td><a href="Samples/TabbedWindowOrdering">Restrict Tabbed and Document window ordering</a></td>
+ </tr>
+  <tr>
   <td><a href="Samples/TabbedWindowOrdering">Tabbed and Document window re-ordering notification</a></td>
+  <td><a href="Samples/DocumentTabOrdering">Restrict TDI window ordering in DocumentContainer</a></td>
+  <td><a href="Samples/DocumentTabOrdering">TDI window re-ordering notification in DocumentContainer</a></td>
  </tr>
 </table>
---
+++ Samples/DocumentTabOrdering/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DocumentTabOrdering
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void documentcontainer1_DocumentTabOrderChanged(object sender, Syncfusion.Windows.Tools.Controls.DocumentTabOrderChangedEventArgs e)
+        {
+            var drag_Drop_Item = e.TargetTabGroup;
+
+            //Get the old and new index of the SourceTabItem
+            var oldIndex = e.OldIndex;
+            var newIndex = e.NewIndex;
+
+            //Get the old and new tab group of the SourceTabItem
+            var sourceTabGroup = e.SourceTabGroup;
+            var targetTabGroup = e.TargetTabGroup;
+        }
+
+        private void documentcontainer1_DocumentTabOrderChanging(object sender, Syncfusion.Windows.Tools.Controls.DocumentTabOrderChangingEventArgs e)
+        {
+            // Restrict the TDI item re-ordering
+            e.Cancel = true;
+        }
+    }
+}
